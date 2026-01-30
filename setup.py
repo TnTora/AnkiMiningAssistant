b642b7540ca790aa3433cc311a3bd2d53f02892c
TnTora <128092613+TnTora@users.noreply.github.com>
2026-01-30 22:38:00 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,26 @@ from setuptools import Extension, setup
 import sys
 
 if sys.platform == "linux":
+    import subprocess
+    pkg_config = subprocess.run(["pkg-config", "--cflags", "--libs", "libpipewire-0.3"], capture_output=True, check=True)
+
+    output = (temp for temp in pkg_config.stdout.decode().split(" "))
+    inc_dirs = []
+    extra_args = ["-fPIC"]
+
+    for opt in output:
+        if opt.startswith("-I"):
+            inc_dirs.append(opt[2:])
+        elif opt.startswith("-"):
+            extra_args.append(opt)
+
     setup(
         ext_modules=[
             Extension(
                 name="pipewire_util",
                 sources = ["lib/pipewire_util.c"],
-                include_dirs = ["/usr/include/pipewire-0.3", "/usr/include/spa-0.2"],
-                extra_compile_args = ["-fPIC", "-D_REENTRANT", "-lpipewire-0.3"],
+                include_dirs = inc_dirs,
+                extra_compile_args = extra_args,
                 libraries = ["pipewire-0.3"]
             ),
         ]
