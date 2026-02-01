81f635474069969fd41a9465ee65f5725c4b073e
TnTora <128092613+TnTora@users.noreply.github.com>
2026-02-01 15:20:50 +0100

    fix config path not existing

--- src/ankiminingassistant/util/database.py
+++ src/ankiminingassistant/util/database.py
@@ -13,6 +13,7 @@ from os import getenv
 
 config_base = getenv("APPDATA") or getenv("XDG_CONFIG_HOME") or "~/.config"
 config_path = Path(config_base).expanduser() / "AnkiMiningAssistant"
+config_path.mkdir(parents=True, exist_ok=True)
 
 
 class GeneralSettings:
